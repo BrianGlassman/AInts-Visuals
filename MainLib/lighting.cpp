cf55df20dd7596b166afc831294018e87d5c4479
Brian Glassman <>
2022-11-06 23:39:23 -0700

    class-ify lighting, although orbiting WIP

--- MainLib/lighting.cpp
+++ MainLib/lighting.cpp
@@ -1,12 +1,106 @@
 #include "lighting.hpp"
 
-#define LIGHT_SRC GL_LIGHT0
-#define AMBIENT GL_LIGHT1
 #define SHINY_DEFAULT 10
 
-float ambient_level = 0.2;
-float diffuse_level = 0.45;
-float specular_level = 0.7;
+int IDs[8] = {
+	GL_LIGHT0,
+	GL_LIGHT1,
+	GL_LIGHT2,
+	GL_LIGHT3,
+	GL_LIGHT4,
+	GL_LIGHT5,
+	GL_LIGHT6,
+	GL_LIGHT7,
+};
+int nextID = 0;
+
+class Light
+{
+public:
+	Light(float ambient = 0, float diffuse = 0, float specular = 0);
+
+	void SetAmbientLevel(float level);
+	void SetDiffuseLevel(float level);
+	void SetSpecularLevel(float level);
+protected:
+	int id;
+	float position[3] = {0, 0, 0};
+
+	void SetLightfv(GLenum pname, float level);
+};
+
+Light::Light(float ambient, float diffuse, float specular)
+{
+	// Assign an ID
+	if (nextID > 7) Fatal(999, "Ran out of lights\n");
+	id = IDs[nextID];
+	nextID++;
+
+	// Setup the light
+	glEnable(id);
+
+	// Translate intensity to color vectors
+	SetAmbientLevel(ambient);
+	SetDiffuseLevel(diffuse);
+	SetSpecularLevel(specular);
+}
+
+void Light::SetLightfv(GLenum pname, float level)
+{
+	float vec[] = {level, level, level, 1.0};
+	glLightfv(id, pname, vec);
+}
+void Light::SetAmbientLevel(float level)
+{
+	SetLightfv(GL_AMBIENT, level);
+}
+void Light::SetDiffuseLevel(float level)
+{
+	SetLightfv(GL_DIFFUSE, level);
+}
+void Light::SetSpecularLevel(float level)
+{
+	SetLightfv(GL_SPECULAR, level);
+}
+
+enum class OrbitMode {
+	orbiting,
+	manual,
+};
+
+class OrbitLight : Light
+{
+public:
+	OrbitLight(float ambient = 0, float diffuse = 0, float specular = 0);
+
+	float speed = 1.0;
+	float radius = 5;
+
+	void DrawLight();
+private:
+	float azimuth, elevation;
+
+	OrbitMode mode = OrbitMode::orbiting;
+};
+
+OrbitLight::OrbitLight(float ambient, float diffuse, float specular)
+{
+	// FIXME constructor chaining needed
+	{
+		// Assign an ID
+		if (nextID > 7) Fatal(999, "Ran out of lights\n");
+		id = IDs[nextID];
+		nextID++;
+
+		// Setup the light
+		glEnable(id);
+
+		// Translate intensity to color vectors
+		SetAmbientLevel(ambient);
+		SetDiffuseLevel(diffuse);
+		SetSpecularLevel(specular);
+	}
+}
 
 void initLighting()
 {
@@ -15,27 +109,60 @@ void initLighting()
 	glEnable(GL_COLOR_MATERIAL);
 	glShadeModel(GL_SMOOTH);
 
-	// Translate intensity to color vectors
-	float Ambient[] = {ambient_level, ambient_level, ambient_level, 1.0};
-	float Diffuse[] = {diffuse_level, diffuse_level, diffuse_level, 1.0};
-	float Specular[] = {specular_level, specular_level, specular_level, 1.0};
-
 	glEnable(GL_NORMALIZE);
 
-	// Setup ambient lighting
-	glEnable(AMBIENT);
-	glLightfv(AMBIENT, GL_AMBIENT, Ambient);
-
-	// Setup directed lighting
-	glEnable(LIGHT_SRC);
-	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, 1.0); // Use eye point for specular reflection
-	glLightfv(LIGHT_SRC, GL_DIFFUSE, Diffuse);
-	glLightfv(LIGHT_SRC, GL_SPECULAR, Specular);
+	// Use eye point for specular reflection
+	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, 1.0); 
 
 	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, SHINY_DEFAULT);
 
 	// Setting color will set ambient and diffuse colors
 	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 
+	Light ambient(0.0, 0, 0);
+	OrbitLight orbiter(0, 0.45, 0.7);
+
 	ErrCheck("initLighting");
+}
+
+void OrbitLight::DrawLight()
+{
+	// Handle the light source
+	if (mode == OrbitMode::orbiting) azimuth -= speed;
+	azimuth = fmod(azimuth, 360.0);
+	float position[] = {
+		radius*Cos(azimuth)*Cos(elevation),
+		radius*Sin(elevation),
+		radius*Sin(azimuth)*Cos(elevation),
+	1.0};
+	glLightfv(id, GL_POSITION, position);
+	
+	// Draw gizmos
+	glPushMatrix(); {
+		glTranslatef(position[0], position[1], position[2]);
+
+		glColor3f(1, 1, 1);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_LINES); {
+			// Draw an unlighted cross at the light source
+			glVertex3d( 0, -1,  0);
+			glVertex3d( 0, +1,  0);
+			glVertex3d(-1,  0,  0);
+			glVertex3d(+1,  0,  0);
+			glVertex3d( 0,  0, -1);
+			glVertex3d( 0,  0, +1);
+			
+			// Draw axis projections to the origin
+			glColor3f(0.6, 0, 0);
+			glVertex3d(0, 0, 0);
+			glVertex3d(-position[0], 0, 0);
+			glColor3f(0, 0, 0.7);
+			glVertex3d(-position[0], 0, 0);
+			glVertex3d(-position[0], 0, -position[2]);
+			glColor3f(0, 0.5, 0);
+			glVertex3d(-position[0], 0, -position[2]);
+			glVertex3d(-position[0], -position[1], -position[2]);
+		} glEnd();
+		glEnable(GL_LIGHTING);
+	} glPopMatrix();
 }
