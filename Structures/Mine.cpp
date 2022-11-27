67230b7253fb21a6f5bc17293fb886fe0022a803
Brian Glassman <>
2022-11-26 18:38:07 -0700

    Mine internal tunnels, but still need interconnection

--- Structures/Mine.cpp
+++ Structures/Mine.cpp
@@ -1,45 +1,123 @@
 #include "Mine.hpp"
 #include "globals.hpp"
 
-void Mine::ApplyNoise(float offset[])
+Mine::Mine(unsigned char sides)
 {
-    // Get the Y-axis noise applied at the center
-    float x = center[0] + offset[0];
-    float y = center[1] + offset[1];
-    float z = center[2] + offset[2];
-    auto centerNoise = noisePtr->getNoise(x, y, z);
+	type = 5;
+	sideType = SideType::chamber;
+	noiseScale = Globals::chamberNoiseScale;
 
-    float scale;
-    bool useCenterNoise;
-    for (unsigned int i = 0; i < baseVertices.size(); i++)
+	// Padding between edge of chamber and edge of cell
+	float padding = 0.1;
+	radius = 0.5 - padding;
+	padScale = 1 - 2*padding;
+
+	panelWidth = 1.0*padScale / panels;
+
+	UnpackSides(sides);
+
+	Create();
+}
+
+void Mine::CreateInternal(int i0, bool f0, int i1, bool f1, int i2, bool f2)
+{
+    // Make arms going across in all directions (even if no exits at the ends)
+
+	float r = 0.5 * padScale;
+	float xAtTunnel = sqrt(r*r - tunnelRadius*tunnelRadius);
+
+    // Maintain the same ratio as external panels
+    int internalPanels;
     {
-		if (armIndices.find(i) == armIndices.end())
-		{ // Main chamber
-            scale = noiseScale;
-            useCenterNoise = true;
-		}
-		else
-		{ // Arm
-			scale = Globals::tunnelNoiseScale;
-            useCenterNoise = false;
-		}
-
-
-        std::vector<float> p;
-        if (useCenterNoise)
-        {
-            p = centerNoise;
-        }
-        else
-        {
-            float x = baseVertices[i][0] + center[0] + offset[0];
-            float y = baseVertices[i][1] + center[1] + offset[1];
-            float z = baseVertices[i][2] + center[2] + offset[2];
-            p = noisePtr->getNoise(x, y, z);
-        }
-        
-		vertices[i][0] = baseVertices[i][0] + p[0]*scale;
-        vertices[i][1] = baseVertices[i][1] + p[1]*scale;
-        vertices[i][2] = baseVertices[i][2] + p[2]*scale;
+        float d = padding / armPanels;
+        internalPanels = round((xAtTunnel - tunnelRadius) / d);
     }
+	float d = (xAtTunnel - tunnelRadius) / internalPanels;
+	float thetaD = 360 / tunnelN;
+
+	Vector3 coords;
+	float x = tunnelRadius, y, z;
+	for (int i = 0; i < internalPanels; i++)
+	{
+		for (float theta = 0; theta < 360; theta += thetaD)
+		{
+			// Top-left quadrant, moving CCW
+			{ // This row
+				Polar2Cart(tunnelRadius, theta, &z, &y);
+				z = -z;
+				coords[i1] = f1 ? -y : y;
+				coords[i2] = f2 ? -z : z;
+
+				// Outer edge
+				coords[i0] = 0;
+				indices.push_back(vertices.size()); quadIndices.push_back(vertices.size());
+				normals.push_back(coords);
+				coords[i0] = (f0 ? -1 : 1) * (x + d);
+				vertices.push_back(coords);
+
+				// Inner edge
+				coords[i0] = 0;
+				indices.push_back(vertices.size()); quadIndices.push_back(vertices.size());
+				normals.push_back(coords);
+				coords[i0] = f0 ? -x : x;
+				vertices.push_back(coords);
+			}
+			{ // Next row
+				Polar2Cart(tunnelRadius, theta + thetaD, &z, &y);
+				z = -z;
+				coords[i1] = f1 ? -y : y;
+				coords[i2] = f2 ? -z : z;
+
+				// Inner edge
+				coords[i0] = 0;
+				indices.push_back(vertices.size()); quadIndices.push_back(vertices.size());
+				normals.push_back(coords);
+				coords[i0] = f0 ? -x : x;
+				vertices.push_back(coords);
+
+				// Outer edge
+				coords[i0] = 0;
+				indices.push_back(vertices.size()); quadIndices.push_back(vertices.size());
+				normals.push_back(coords);
+				coords[i0] = (f0 ? -1 : 1) * (x + d);
+				vertices.push_back(coords);
+			}
+		}
+
+		x += d;
+	}
+}
+void Mine::Create()
+{
+	PreCreate();
+
+	// fprintf(stdout, "panelWidth = %f\n", panelWidth);
+
+	// Note: flip i0 to change winding direction
+
+	// Faces
+	if (true)
+	{
+		CreateFace(0, false, 1, false, 2, false, right);
+		CreateFace(1, false, 2, false, 0, false, top);
+		CreateFace(2, false, 0, false, 1, false, forward);
+		CreateFace(0,  true, 1, false, 2,  true, left);
+		CreateFace(1,  true, 2, false, 0,  true, bottom);
+		CreateFace(2,  true, 0, false, 1,  true, back);
+	}
+
+    // Internals
+    if (true)
+    {
+		CreateInternal(0, false, 1, false, 2, false); // right
+		CreateInternal(1, false, 2, false, 0, false); // top
+		CreateInternal(2, false, 0, false, 1, false); // forward
+		CreateInternal(0,  true, 1, false, 2,  true); // left
+		CreateInternal(1,  true, 2, false, 0,  true); // bottom
+		CreateInternal(2,  true, 0, false, 1,  true); // back
+    }
+
+	ErrCheck("Chamber::Create\n");
+
+	PostCreate();
 }
--- Structures/Mine.hpp
+++ Structures/Mine.hpp
@@ -5,8 +5,10 @@
 class Mine : public Chamber
 {
 public:
-    Mine() : Chamber() {}
-    Mine(unsigned char sides) : Chamber(sides) {}
-    
-    void ApplyNoise(float offset[]);
+    Mine() : Mine(63) {}
+    // FIXME temporary convenience until auto-generating is implemented
+    Mine(unsigned char sides);
+
+    void CreateInternal(int i0, bool f0, int i1, bool f1, int i2, bool f2);
+    virtual void Create();
 };
